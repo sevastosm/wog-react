35034b6dd5dfbd20333ab74ac2c82b10aee526f1
sevastosm <sevastosmatzouranis@gmail.com>
2019-10-05 19:06:49 +0300

    Fixes auto complete logic

--- src/components/SpeakersAutoComplete.js
+++ src/components/SpeakersAutoComplete.js
@@ -1,178 +1,49 @@
 import React from 'react';
-import deburr from 'lodash/deburr';
-import Autosuggest from 'react-autosuggest';
-import match from 'autosuggest-highlight/match';
-import parse from 'autosuggest-highlight/parse';
-import TextField from '@material-ui/core/TextField';
-import Paper from '@material-ui/core/Paper';
-import MenuItem from '@material-ui/core/MenuItem';
-import Popper from '@material-ui/core/Popper';
-import { makeStyles } from '@material-ui/core/styles';
+import Select from 'react-select';
+import { element } from 'prop-types';
 
-const suggestions = [
-  { label: 'Κοροβέσης Γιώργος' },
-  { label: 'Wissam Zein' },
-  { label: 'Βασίλης Καβίδας' },
-  { label: 'Βάιος Κωστέλλος' },
-  { label: 'Χάρης Καραγιάννης' },
+const options = [
+  { value: '1', label: 'Κοροβέσης Γιώργος' },
+  { value: '2', label: 'Wissam Zein' },
+  { value: '3', label: 'Βασίλης Καβίδας' },
+  { value: '4', label: 'Βάιος Κωστέλλος' },
+  { value: '5', label: 'Χάρης Καραγιάννης' },
+
+
 ];
 
-function renderInputComponent(inputProps) {
-  const { classes, inputRef = () => {}, ref, ...other } = inputProps;
-
-  return (
-    <TextField
-      fullWidth
-      InputProps={{
-        inputRef: node => {
-          ref(node);
-          inputRef(node);
-        },
-        classes: {
-          input: classes.input,
-        },
-      }}
-      {...other}
-    />
-  );
-}
-
-function renderSuggestion(suggestion, { query, isHighlighted }) {
-  const matches = match(suggestion.label, query);
-  const parts = parse(suggestion.label, matches);
-
-  return (
-    <MenuItem selected={isHighlighted} component="div">
-      <div>
-        {parts.map(part => (
-          <span key={part.text} style={{ fontWeight: part.highlight ? 500 : 400 }}>
-            {part.text}
-          </span>
-        ))}
-      </div>
-    </MenuItem>
-  );
-}
-
-function getSuggestions(value) {
-  const inputValue = deburr(value.trim()).toLowerCase();
-  const inputLength = inputValue.length;
-  let count = 0;
-
-  return inputLength === 0
-    ? []
-    : suggestions.filter(suggestion => {
-        const keep =
-          count < 5 && suggestion.label.slice(0, inputLength).toLowerCase() === inputValue;
-
-        if (keep) {
-          count += 1;
-        }
-
-        return keep;
-      });
-}
-
-function getSuggestionValue(suggestion) {
-  return suggestion.label;
-}
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    height: 250,
-    flexGrow: 1,
-  },
-  container: {
-    position: 'relative',
-  },
-  suggestionsContainerOpen: {
-    position: 'absolute',
-    zIndex: 1,
-    marginTop: theme.spacing(1),
-    left: 0,
-    right: 0,
-  },
-  suggestion: {
-    display: 'block',
-  },
-  suggestionsList: {
-    margin: 0,
-    padding: 0,
-    listStyleType: 'none',
-  },
-  divider: {
-    height: theme.spacing(2),
-  },
-}));
-
-export default function IntegrationAutosuggest() {
-  const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const [state, setState] = React.useState({
-    single: '',
-    popper: '',
-  });
-
-  const [stateSuggestions, setSuggestions] = React.useState([]);
-
-  const handleSuggestionsFetchRequested = ({ value }) => {
-    setSuggestions(getSuggestions(value));
+class SpeakersAutoComplete extends React.Component {
+  state = {
+    selectedOption: null,
+    openMenu:false
   };
-
-  const handleSuggestionsClearRequested = () => {
-    setSuggestions([]);
+  handleChange = selectedOption => {
+    this.setState({ selectedOption:selectedOption,menuIsOpen:true });
+    console.log(`Option selected:`, selectedOption);
   };
+  handleInputChange = (query, { action }) => {
+    if (action === "input-change"&&query!=="") {
+        this.setState({ openMenu: true });
+    }else{
+      this.setState({ openMenu: false });
+    }
+};
+hideMenu = () => {
+this.setState({ openMenu: false });
+};
+  render() {
+    const { selectedOption,openMenu } = this.state;
 
-  const handleChange = name => (event, { newValue }) => {
-    setState({
-      ...state,
-      [name]: newValue,
-    });
-  };
-
-  const autosuggestProps = {
-    renderInputComponent,
-    suggestions: stateSuggestions,
-    onSuggestionsFetchRequested: handleSuggestionsFetchRequested,
-    onSuggestionsClearRequested: handleSuggestionsClearRequested,
-    getSuggestionValue,
-    renderSuggestion,
-  };
-
-  return (
-    <div className={classes.root}>
-      <Autosuggest
-        {...autosuggestProps}
-        inputProps={{
-          classes,
-          id: 'react-autosuggest-popper',
-          label: 'Country',
-          placeholder: 'With Popper',
-          value: state.popper,
-          onChange: handleChange('popper'),
-          inputRef: node => {
-            setAnchorEl(node);
-          },
-          InputLabelProps: {
-            shrink: true,
-          },
-        }}
-        theme={{
-          suggestionsList: classes.suggestionsList,
-          suggestion: classes.suggestion,
-        }}
-        renderSuggestionsContainer={options => (
-          <Popper anchorEl={anchorEl} open={Boolean(options.children)}>
-            <Paper
-              square
-              {...options.containerProps}
-              style={{ width: anchorEl ? anchorEl.clientWidth : undefined }}
-            >
-              {options.children}
-            </Paper>
-          </Popper>
-        )}
+    return (
+      <Select id="mainSearch"
+        value={selectedOption}
+        onInputChange={this.handleInputChange}
+        onChange={this.hideMenu}
+        options={options}
+        placeholder ={'Select Speaker '}
+        menuIsOpen={openMenu}
       />
-    </div>
-  );
+    );
+  }
 }
+export default SpeakersAutoComplete
