53457079d873c7d8c1c34a8a3259d49ed3220402
Nick Bountalis <nikosmpountalis@gmail.com>
2021-03-16 22:24:01 +0200

    Added Fade in

--- src/components/filter/Filter.js
+++ src/components/filter/Filter.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Collapse } from 'reactstrap'
+import { Button, Collapse, Fade } from 'reactstrap'
 import {
     SelectDates,
     SelectSermon,
@@ -16,7 +16,7 @@ const defaultValues = {
     sermonsIds: null
 }
 
-export default function Filter({ lang = '' }) {
+export default function Filter({ lang = 'en' }) {
     const [selectedFilters, setSelectedFilters] = React.useState({
         ...defaultValues
     })
@@ -30,34 +30,47 @@ export default function Filter({ lang = '' }) {
         setSelectedFilters({ ...defaultValues })
     }, [])
 
+    const onApplyFilters = React.useCallback(() => {}, [])
+
     return (
         <>
             <Button onClick={toggle} className="filter-btn">
                 Filters
             </Button>
             <Collapse isOpen={isOpen} className="filter-collapsibles">
-                <SelectDates
-                    dates={{
-                        dateFrom: selectedFilters.dateFrom,
-                        dateTo: selectedFilters.dateTo
-                    }}
-                    lang={lang}
-                    setFilters={setSelectedFilters}
-                />
-                <Searchbar setFilters={setSelectedFilters} />
-                <SelectSpeaker
-                    values={selectedFilters.speakersIds}
-                    lang={lang}
-                    setFilters={setSelectedFilters}
-                />
-                <SelectSermon
-                    values={selectedFilters.sermonsIds}
-                    lang={lang}
-                    setFilters={setSelectedFilters}
-                />
-                <Button onClick={resetSelectedFilters} className="reset-filters-btn">
-                    Reset Filters
-                </Button>
+                <Fade in={isOpen}>
+                    <SelectDates
+                        dates={{
+                            dateFrom: selectedFilters.dateFrom,
+                            dateTo: selectedFilters.dateTo
+                        }}
+                        lang={lang}
+                        setFilters={setSelectedFilters}
+                    />
+                    <Searchbar setFilters={setSelectedFilters} />
+                    <SelectSpeaker
+                        values={selectedFilters.speakersIds}
+                        lang={lang}
+                        setFilters={setSelectedFilters}
+                    />
+                    <SelectSermon
+                        values={selectedFilters.sermonsIds}
+                        lang={lang}
+                        setFilters={setSelectedFilters}
+                    />
+                    <Button
+                        onClick={resetSelectedFilters}
+                        className="reset-filters-btn"
+                    >
+                        Reset Filters
+                    </Button>
+                    <Button
+                        onClick={onApplyFilters}
+                        className="reset-filters-btn"
+                    >
+                        Apply Filters
+                    </Button>
+                </Fade>
             </Collapse>
         </>
     )
