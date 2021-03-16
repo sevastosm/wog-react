76fa1579a2dcce8bb9923ae20278a6d35bee82cc
Nick Bountalis <nikosmpountalis@gmail.com>
2021-03-16 20:54:38 +0200

    refactored to async await

--- src/api/apis.js
+++ src/api/apis.js
@@ -1,9 +1,7 @@
-const host = "http://wordofgod.gr";
-const getData = (url) => {
-  fetch(`${host}${url}`)
-    .then((response) => response.json())
-    .then((data) => console.log(url,data));
-};
-
+const host = 'http://wordofgod.gr'
+const getData = async url => {
+    const response = await fetch(`${host}${url}`)
+    const data = await response.json()
+    return data
+}
 export default getData
-
