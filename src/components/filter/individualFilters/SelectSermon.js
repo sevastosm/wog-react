1d720ef74d05183a9d6dd9418627287fd177d3c5
Nick Bountalis <nikosmpountalis@gmail.com>
2021-03-16 22:24:35 +0200

    Created Sellect All  sermons or speakers choice

--- src/components/filter/individualFilters/SelectSermon.js
+++ src/components/filter/individualFilters/SelectSermon.js
@@ -3,6 +3,45 @@ import Select from 'react-select'
 import getData from '../../../api/apis'
 import { endPoints } from '../../../constants'
 
+function createAllSermonsChoice(lang) {
+    const data = {}
+    switch (lang) {
+        case 'gr':
+            data.value = '900'
+            data.label = 'Όλες οι εκπομπές'
+            break
+        case 'es':
+            data.value = '901'
+            data.label = 'Buscar todas las series'
+            break
+        case 'it':
+            data.value = '902'
+            data.label = 'Search All Series'
+            break
+        case 'ro':
+            data.value = '903'
+            data.label = 'Toate emisiunile'
+            break
+        case 'ru':
+            data.value = '904'
+            data.label = 'Все Передачи'
+            break
+        case 'pl':
+            data.value = '905'
+            data.label = 'Wszystkie programy TV'
+            break
+        case 'fr':
+            data.value = '906'
+            data.label = 'Toutes les émissions'
+            break
+        default:
+            data.value = '907'
+            data.label = 'Search All Series'
+    }
+
+    return data
+}
+
 function formatSermon(sermon) {
     if (sermon.Name === '(Removed)') {
         return ''
@@ -24,6 +63,7 @@ export default function SelectSermon({ values, lang, setFilters }) {
     const getAndSetSermons = React.useCallback(async () => {
         const data = await fetchSermons(lang)
         const formatedData = data.Data.map(formatSermon)
+        formatedData.unshift(createAllSermonsChoice(lang))
         setSermons(formatedData)
     }, [lang])
 
@@ -38,8 +78,10 @@ export default function SelectSermon({ values, lang, setFilters }) {
     )
 
     React.useEffect(() => {
-        getAndSetSermons()
-    }, [getAndSetSermons])
+        getAndSetSermons(lang)
+    }, [getAndSetSermons, lang])
 
-    return <Select options={sermons} isMulti onChange={onChange} value={values}/>
+    return (
+        <Select options={sermons} isMulti onChange={onChange} value={values} />
+    )
 }
--- src/components/filter/individualFilters/SelectSpeaker.js
+++ src/components/filter/individualFilters/SelectSpeaker.js
@@ -3,6 +3,45 @@ import Select from 'react-select'
 import getData from '../../../api/apis'
 import { endPoints } from '../../../constants'
 
+function createAllSpeakersChoice(lang) {
+    const data = {}
+    switch (lang) {
+        case 'gr':
+            data.value = '800'
+            data.label = 'Όλοι οι ομιλητές'
+            break
+        case 'es':
+            data.value = '801'
+            data.label = 'Todos los oradores'
+            break
+        case 'it':
+            data.value = '802'
+            data.label = 'All Speakers'
+            break
+        case 'ro':
+            data.value = '803'
+            data.label = 'Toţi vorbitorii'
+            break
+        case 'ru':
+            data.value = '804'
+            data.label = 'Все Проповедники'
+            break
+        case 'pl':
+            data.value = '805'
+            data.label = 'All Speakers'
+            break
+        case 'fr':
+            data.value = '806'
+            data.label = 'All Speakers'
+            break
+        default:
+            data.value = '807'
+            data.label = 'All Speakers'
+    }
+
+    return data
+}
+
 function formatSpeaker(speaker) {
     if (speaker.Name === '(Removed)') {
         return ''
@@ -24,6 +63,7 @@ export default function SelectSpeaker({ values, lang, setFilters }) {
     const getAndSetSpeakers = React.useCallback(async () => {
         const data = await fetchSpeaker(lang)
         const formatedData = data.Data.map(formatSpeaker)
+        formatedData.unshift(createAllSpeakersChoice(lang))
         setSpeakers(formatedData)
     }, [lang])
 
